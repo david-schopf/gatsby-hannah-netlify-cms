11ca762294f79e01affad27f1cc80b8d5dc03d45
David Schopf <david.schopf@jambit.com>
2019-11-16 16:14:58 +0100

    Rename Blog-Post to Project

--- src/templates/project.js
+++ src/templates/project.js
@@ -54,7 +54,7 @@ BlogPostTemplate.propTypes = {
   helmet: PropTypes.object,
 }
 
-const BlogPost = ({ data }) => {
+const Project = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
@@ -79,13 +79,13 @@ const BlogPost = ({ data }) => {
   )
 }
 
-BlogPost.propTypes = {
+Project.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
   }),
 }
 
-export default BlogPost
+export default Project
 
 export const pageQuery = graphql`
   query BlogPostByID($id: String!) {
