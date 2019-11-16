c24b07dec834d2bda8bbd7c3222263de42cb1e04
David Schopf <david.schopf@jambit.com>
2019-11-16 17:04:50 +0100

    Enable local netlify development

--- src/cms/preview-templates/ProjectPagePreview.js
+++ src/cms/preview-templates/ProjectPagePreview.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/project'
+import {ProjectTemplate} from '../../templates/project'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
+const ProjectPagePreview = ({entry, widgetFor}) => (
+    <ProjectTemplate
     content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
   />
-)
+);
 
-BlogPostPreview.propTypes = {
+ProjectPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
-}
+};
 
-export default BlogPostPreview
+export default ProjectPagePreview
---
+++ src/pages/projects/finding-freddy.md
@@ -0,0 +1,12 @@
+---
+templateKey: project
+title: Finding freddy
+date: 2019-12-17T15:04:10.000Z
+featuredpost: false
+featuredimage: /img/flavor_wheel.jpg
+description: Theater von Hannah
+tags:
+  - text
+  - theater
+---
+# Finding Freddy
