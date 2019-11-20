c30a68dab38967efcdfa5aa000dff7dd45cfc826
David Schopf <david.schopf@jambit.com>
2019-11-20 22:45:18 +0100

    move static images

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,27 +16,21 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={`${withPrefix('/')}apple-touch-icon.png`}
         />
         <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
+            rel="icon"
+            type="image/png"
+            href={`${withPrefix('/')}favicon-32x32.png`}
+            sizes="32x32"
         />
         <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
-
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
+            rel="icon"
+            type="image/png"
+            href={`${withPrefix('/')}favicon-16x16.png`}
+            sizes="16x16"
         />
 
         <link href="https://fonts.googleapis.com/css?family=BarlowPlayfair+Display&display=swap" rel="stylesheet"/>
