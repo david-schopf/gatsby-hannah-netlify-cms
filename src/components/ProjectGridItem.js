e6e4f9d969be86c92a5273bd6081b4ba9612e91c
David Schopf <david.schopf@jambit.com>
2020-01-05 00:36:35 +0100

    fix mobile view of projects

--- src/components/ProjectGridItem.js
+++ src/components/ProjectGridItem.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import React from "react";
 
 export function ProjectGridItem(props) {
-    return <article className={`project-item`}
+    return <article className="project-item"
     >
         <Link to={props.post.fields.slug}>
             <header>
